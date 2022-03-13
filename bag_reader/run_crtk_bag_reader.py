92a7ace42d17599eb8fc3288180ae95a35e8c8bf
Your Name <you@example.com>
2022-03-13 15:32:06 -0400

    customized files for MHC raven

--- bag_reader/run_crtk_bag_reader.py
+++ bag_reader/run_crtk_bag_reader.py
@@ -4,8 +4,8 @@ import func_raven_bag_reader as reader
 bag_name = 'recorder_CRTKmeasurejs.bag'
 result_name = bag_name[:-3] + 'csv'
 
-bag_path = 'bag_files'
-result_path = 'result_files'
+bag_path = '/home/holyoke/raven2_CRTK_Python_controller/bag_reader/rosbags'
+result_path = '/home/holyoke/raven2_CRTK_Python_controller/bag_reader/rosbags'
 
 
 reader.read_bag_CRTK_measured_js(bag_name = bag_path + '/' + bag_name, 
